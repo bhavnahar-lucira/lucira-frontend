52eb6b7e46d3ee7f3c02b34fcc8ad87f2c5af131
Lucira Jewelry <sumitchavda04@gmail.com>
2026-06-08 14:20:46 +0530

    change

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,8 +25,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // const isProd = process.env.NODE_ENV === "production";
-  const isProd = true; // Set to true for testing purposes, change to false in development
+  const isProd = process.env.NODE_ENV === "production";
+
   return (
     <html lang="en">
       <head>
