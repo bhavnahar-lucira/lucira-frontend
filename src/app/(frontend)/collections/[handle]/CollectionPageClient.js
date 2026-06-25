be222498eb5c04f023c70e2a35f19d6a0eace8dd
Lucira Jewelry <harshtandel103@gmail.com>
2026-06-25 11:54:13 +0530

    Update CollectionPageClient.js

--- src/app/(frontend)/collections/[handle]/CollectionPageClient.js
+++ src/app/(frontend)/collections/[handle]/CollectionPageClient.js
@@ -636,7 +636,15 @@ export default function CollectionPage({ params: paramsPromise, initialData }) {
                 <div className="flex items-center gap-2"><Image src="https://cdn.shopify.com/s/files/1/0739/8516/3482/files/hugeicons_delivery-return-01.png" alt="Return" width={20} height={20} className="md:w-6" /><span>15-day Money Back Guarantee</span></div>
               </div>
             </div>
-            <div className="flex-1 relative w-full h-57.5"><Image src="https://cdn.shopify.com/s/files/1/0739/8516/3482/files/PLP_Banner.jpg" alt={displayTitle} fill className="object-cover" /></div>
+            <div className="flex-1 w-full h-auto">
+              <Image 
+                src="https://cdn.shopify.com/s/files/1/0739/8516/3482/files/PLP_Banner.jpg" 
+                alt={displayTitle} 
+                width={640} 
+                height={223} 
+                className="w-full h-auto object-contain" 
+              />
+            </div>
           </div>
         </div>
       )}
