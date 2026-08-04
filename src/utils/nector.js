39be486f91d208877137cc224af45ffec66e0bd4
Lucira Jewelry <harshtandel103@gmail.com>
2026-08-04 16:32:13 +0530

    perfomance-optimiztion

--- src/utils/nector.js
+++
@@ -1,4 +0,0 @@
-import { fetchNectorReviews as fetchLib, loadNectorReviews as loadLib } from "@/lib/nector";
-
-export const fetchNectorReviews = fetchLib;
-export const loadNectorReviews = loadLib;
