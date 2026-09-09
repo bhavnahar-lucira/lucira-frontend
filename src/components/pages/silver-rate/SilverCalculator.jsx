e019a4fa6606e2e0dbb6d5fb08411d6d65eaa959
bhavnahar-lucira <bhav.nahar@lucirajewelry.com>
2026-09-09 18:57:02 +0530

    Revert "Sumit 0809"

--- src/components/pages/silver-rate/SilverCalculator.jsx
+++ src/components/pages/silver-rate/SilverCalculator.jsx
@@ -2,6 +2,6 @@
 
 import SophisticatedMetalCalculator from "../SophisticatedMetalCalculator";
 
-export default function SilverCalculator({ cityName, stateName, isStatePage }) {
-  return <SophisticatedMetalCalculator initialMetal="silver" initialCity={cityName} initialState={stateName} isStatePage={isStatePage} />;
+export default function SilverCalculator({ cityName }) {
+  return <SophisticatedMetalCalculator initialMetal="silver" initialCity={cityName} />;
 }
